49d0b89949ff3cd6e11bbec3c2e032f6f9227069
gag-dan <gagikdanielian@gmail.com>
2021-02-16 14:49:17 +0400

    Create main.py
    v1

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+
+directory = os.getcwd()
+
+files = os.listdir(directory)
+print(files)
+
+
+def delete_mark():
+    for file in files:
+        if file.endswith('.svg'):
+            path = file
+            f = open(path, 'r')
+            data = f.read()
+            j, k = 0, 0
+            for i in range(len(data)):
+                if data[i] == '<' and data[i + 1] == 't' and data[i + 2] == 'e':
+                    if j == 0:
+                        j = i
+                    else:
+                        continue
+                if data[i] == '>' and data[i - 1] == 't' and data[i - 2] == 'x':
+                    k = i + 1
+            text = data[j: k]
+            upd_data = data.replace(text, '')
+            f = open(path, 'w')
+            f.write(upd_data)
+            f.close()
+        else:
+            continue
+
+delete_mark()
+
